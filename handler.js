7b2e68d589bd18014b7eebc66afb37a20a084a1f
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2019-01-09 10:43:00 -0500

    Prettier Extension Edits

--- handler.js
+++ handler.js
@@ -1,32 +1,34 @@
-'use strict';
+"use strict"
 
 module.exports.hello = async (event, context) => {
-
   // GET Request Handler
   // https://ld1smpeg50.execute-api.us-east-1.amazonaws.com/dev/users?name=Rohito
-  if(event.queryStringParameters){
-    return  {
-      statusCode:200,
-      body: JSON.stringify(
-         `Wassssup ${event.queryStringParameters.name}! Serverless Architecture!`
-      )
+  if (event.queryStringParameters) {
+    console.log("EEVENT", event)
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `Wassssup ${
+          event.queryStringParameters.name
+        }! Serverless Architecture! ${event}`
+      })
     }
   }
 
   // POST Request Handler
 
-  if(event.body){
+  if (event.body) {
     event.body = JSON.parse(event.body)
-    return{
-      statusCode:200,
-      body: JSON.stringify({message: event.body,
-      status: "Success"})
-    }
-  }
-  else{
-    console.log({event})
     return {
-      statusCode:404,
+      statusCode: 200,
+      body: JSON.stringify({
+        message: event.body,
+        status: "Success"
+      })
+    }
+  } else {
+    return {
+      statusCode: 404,
       body: JSON.stringify({
         message: `Something went wrong!`
       })
@@ -35,4 +37,4 @@ module.exports.hello = async (event, context) => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+}
